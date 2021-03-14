71573854edbd707c94fc82d5cfc49c1b47626dfa
aliasad059 <aliasad059@gmail.com>
2021-03-14 15:31:43 +0330

    HW2 is done

--- Assignments/2/Codes/Encoder.vhd
+++ Assignments/2/Codes/Encoder.vhd
@@ -1,3 +1,4 @@
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
  
@@ -14,6 +15,7 @@ use IEEE.STD_LOGIC_1164.all;
 --       ZZZZ		    ZZ		    0
 --
 --------------------------------------------------
+
 entity Encoder is
  	port(
  		input : in STD_LOGIC_VECTOR(3 downto 0);
@@ -26,12 +28,14 @@ begin
  
 process(input)
 begin
-	if (input(3) = '1') then output <= "111";
+
+	if    (input(3) = '1') then output <= "111";
 	elsif (input(2) = '1') then output <= "101";
 	elsif (input(1) = '1') then output <= "011";
 	elsif (input(0) = '1') then output <= "001";
-	else output <= "000"
-end process;
- 
-end behave;
+	else 			    		output <= "000";
+	end if;
 
+end process;
+
+end behave;
